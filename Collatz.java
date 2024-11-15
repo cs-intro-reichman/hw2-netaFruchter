2d316bef2e21941d35975c8347fba91233282fdc
Neta Fruchter <netafruchter@Netas-MBP.lan>
2024-11-15 10:40:46 +0200

    first try

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,30 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int seed = Integer.parseInt(args[0]);
+		String mode = args[1];
+		
+		System.out.println("1 4 2 1 (4)");
+		if (mode.equals("v")){
+			for (int i = 2; i <= seed; i++){
+				System.out.print(i + " ");
+				int current_num = i;
+				int counter = 1;
+				while (current_num != 1) {
+					if (current_num%2 == 0) {
+						current_num /= 2;
+					}
+					else {
+						current_num = (current_num*3)+1;
+					}
+					System.out.print(current_num + " ");	
+					counter++;
+				}
+				System.out.println("(" + counter + ")");
+			}
+			System.out.println("");
+		}
+		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,31 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int min_to_add = Integer.parseInt(args[1]); 
+
+        int total_min = (hours * 60) + minutes + min_to_add;
+        int total_hours = total_min / 60;
+        int new_hours = total_hours % 24;
+        int new_minutes = total_min - (total_hours*60);
+
+        String final_min = "";
+        String final_hours = "";
+
+        if (new_minutes < 10){
+            final_min = "0"+new_minutes;
+        }
+        else {
+            final_min = ""+new_minutes;
+        }
+
+        if (new_hours < 10){
+            final_hours = "0"+new_hours;
+        }
+        else {
+            final_hours = ""+new_hours;
+        }
+
+        System.out.println(final_hours + ":" + final_min);
     }
 }
